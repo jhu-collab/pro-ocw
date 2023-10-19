6e023333a7e42c33aa93b022fea973dfbb642559
Elva Shang <ruoyanshang@gmail.com>
2023-10-18 23:03:55 -0400

    init data modules

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MembersModule } from './members/members.module';
+import { UsersModule } from './users/users.module';
+import { InvitesModule } from './invites/invites.module';
+import { CoursesModule } from './courses/courses.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [MembersModule, UsersModule, InvitesModule, CoursesModule],
 })
 export class AppModule {}
